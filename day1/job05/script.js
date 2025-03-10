be2f45dde6c58c4a914e755c02670c8fd7d22a93
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-03-10 18:34:54 +0100

    test: do a fnction with a table who show you the days one par one

---
+++ day1/job05/script.js
@@ -0,0 +1,17 @@
+function showDaysWeeks() {
+  const daysWeeks = [
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+    "Dimanche",
+  ];
+
+  for (let i = 0; i < daysWeeks.length; i++) {
+    console.log(daysWeeks[i]);
+  }
+}
+
+showDaysWeeks();
